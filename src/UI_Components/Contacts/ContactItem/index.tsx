d5b9d774825cbffa4a009ffde199a8a9df80c265
maksim.samorukov <maksim.samorukov@sphaera.ru>
2025-06-01 23:11:54 +0300

    added some content and map

--- src/UI_Components/Contacts/ContactItem/index.tsx
+++ src/UI_Components/Contacts/ContactItem/index.tsx
@@ -70,6 +70,28 @@ export const ContactItem: React.FC<ContactItemProps> = ({
           </Link>
         </div>
       );
+    case "address":
+      return (
+        <div key={id} className={s.itemContainer}>
+          <div className={s.itemLabel}>{label}</div>
+          <div className={s.itemDescription}>{description}</div>
+        </div>
+      );
+
+    case "map":
+      return (
+        <div key={id} className={s.itemContainer}>
+          <div className={s.itemLabel}>{label}</div>
+          <div className={s.itemDescriptionMap}>
+            <iframe
+              src="https://yandex.ru/map-widget/v1/?indoorLevel=1&ll=37.882545%2C55.789066&mode=whatshere&utm_campaign=desktop&utm_medium=search&utm_source=maps&whatshere%5Bpoint%5D=37.880508%2C55.788424&whatshere%5Bzoom%5D=17&z=16.87"
+              width="300"
+              height="150"
+              allowFullScreen={false}
+            ></iframe>
+          </div>
+        </div>
+      );
   }
   return (
     <div key={id} className={s.itemContainer}>
--- src/UI_Components/Contacts/index.tsx
+++ src/UI_Components/Contacts/index.tsx
@@ -10,15 +10,43 @@ export const Contacts = () => {
         <div className={s.headerLabel}>Контакты</div>
       </div>
       <div className={s.bodyContainer}>
-        {contacts.map(({ id, label, description, url }) => (
+        <div className={s.bodyContainerColumnFirst}>
+          {contacts.map(({ id, label, description, url }) => (
+            <ContactItem
+              key={id}
+              id={id}
+              label={label}
+              description={description}
+              url={url}
+            />
+          ))}
+        </div>
+        <div className={s.bodyContainerColumnSecond}>
           <ContactItem
-            key={id}
-            id={id}
-            label={label}
-            description={description}
-            url={url}
+            id="address"
+            label="Адрес студии 3D печати:"
+            description="Балашиха, ул. Ситникова, 4, пом. 11"
           />
-        ))}
+          <div className={s.workingScheduleContainer}>
+            <div className={s.workingScheduleHeader}>Время работы студии:</div>
+            <div className={s.workingScheduleBody}>
+              <div className={s.workingScheduleItem}>
+                Пн-Пт 9:00 - 19:00 (для посещения)
+              </div>
+              <div className={s.workingScheduleItem}>
+                Пн-Пт 9:00 - 19:00 (для расчетов)
+              </div>
+              <div className={s.workingScheduleItem}>
+                Сб-Вс по-договоренности
+              </div>
+            </div>
+          </div>
+          <ContactItem
+            id="map"
+            label="Адрес студии 3D печати:"
+            description="Балашиха, ул. Ситникова, 4, пом. 11"
+          />
+        </div>
       </div>
     </div>
   );
