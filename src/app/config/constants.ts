37b554d6c4a9afa30408e375d1269442613d2939
maksim.samorukov <maxim.samorukov@gmail.com>
2025-11-15 18:58:35 +0300

    added INN to the website

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,9 +1,9 @@
 export const menuItemsLib = {
-  ["/3d_printing"]: "3D-печать",
-  ["/plastic"]: "Типы пластика",
-  ["/price"]: "Стоимость",
-  ["/logistic"]: "Доставка и оплата",
-  ["/contacts"]: "Контакты",
+  ['/3d_printing']: '3D-печать',
+  ['/plastic']: 'Типы пластика',
+  ['/price']: 'Стоимость',
+  ['/logistic']: 'Доставка и оплата',
+  ['/contacts']: 'Контакты',
 };
 export const menuItems = Object.entries(menuItemsLib).map(([href, label]) => ({
   href,
@@ -11,30 +11,30 @@ export const menuItems = Object.entries(menuItemsLib).map(([href, label]) => ({
 }));
 
 export const logoText =
-  "3D Printing Studio — оперативное, профессиональное и надёжное изготовление изделий на заказ методом 3D-печати пластиками.";
+  '3D Printing Studio — оперативное, профессиональное и надёжное изготовление изделий на заказ методом 3D-печати пластиками.\nСаморуков М.Л.\nИНН 402708598897';
 
 export const footerMenu = [
   {
-    label: "Разделы меню",
-    value: "baseMenuItems",
+    label: 'Разделы меню',
+    value: 'baseMenuItems',
   },
   {
-    label: "Контакты",
-    value: "contacts",
+    label: 'Контакты',
+    value: 'contacts',
   },
   {
-    label: "Социальные сети",
-    value: "socials",
+    label: 'Социальные сети',
+    value: 'socials',
   },
 ];
 
 export const socials = [
   {
-    id: "tg",
-    description: "https://t.me/M_Camopykoff",
+    id: 'tg',
+    description: 'https://t.me/M_Camopykoff',
   },
   {
-    id: "vk",
-    description: "https://vk.com/club230694678",
+    id: 'vk',
+    description: 'https://vk.com/club230694678',
   },
 ];
--- src/widgets/common/ui/Footer/index.tsx
+++ src/widgets/common/ui/Footer/index.tsx
@@ -1,19 +1,21 @@
-import Image from "next/image";
-import Link from "next/link";
-import s from "./style.module.scss";
-import { FooterMenuItem } from "@/shared/common/FooterMenuItem";
-import { footerMenu, logoText } from "@/app/config/constants";
+import Image from 'next/image';
+import Link from 'next/link';
+import s from './style.module.scss';
+import { FooterMenuItem } from '@/shared/common/FooterMenuItem';
+import { footerMenu, logoText } from '@/app/config/constants';
 
 export default function Footer() {
   return (
     <div className={s.container}>
       <div className={s.inner_container}>
         <div className={s.logoImg_container}>
-          <Link href={"/3d_printing"}>
+          <Link href={'/3d_printing'}>
             <Image src="/logo.svg" width={100} height={100} alt="logo" />
           </Link>
           <div className={s.logoImg_logo}>
-            <span>{logoText}</span>
+            {logoText.split('\n').map((l, i) => (
+              <span key={i}>{l}</span>
+            ))}
           </div>
         </div>
         <div className={s.baseMenuContainer}>
