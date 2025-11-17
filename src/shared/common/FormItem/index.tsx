e7013262a428ddf263916a5f43d7742132fcec21
maksim.samorukov <maxim.samorukov@gmail.com>
2025-11-17 17:32:01 +0300

    FormItem fixes

--- src/shared/common/FormItem/index.tsx
+++ src/shared/common/FormItem/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { FormItemType } from './types';
+import cn from 'classnames';
 import { InnerFormItem } from './InnerFormItem';
 import s from './style.module.scss';
 
 export const FormItem: React.FC<{
   field: FormItemType;
 }> = ({ field }) => {
+  const isLabel = !!field.label?.length;
+
   return (
-    <div className={s.fieldContainer}>
-      <div className={s.fieldLabel}>
-        <span>{field.label}</span>
-      </div>
+    <div
+      className={cn(s.fieldContainer, !isLabel && s.fieldContainerWithNoLabel)}
+    >
+      {isLabel ? (
+        <div className={s.fieldLabel}>
+          <span>{field.label}</span>
+        </div>
+      ) : (
+        <></>
+      )}
       <InnerFormItem field={field} />
     </div>
   );
