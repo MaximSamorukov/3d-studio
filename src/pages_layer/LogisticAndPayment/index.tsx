bf0911f6baa417d42216e77954abaee7fe823538
maksim.samorukov <maxim.samorukov@gmail.com>
2025-11-21 17:27:00 +0300

    pages_layer -> LogisticAndPayment

--- src/pages_layer/LogisticAndPayment/index.tsx
+++ src/pages_layer/LogisticAndPayment/index.tsx
@@ -1,6 +1,6 @@
-import { Logistics } from "@/widgets/common/ui/LogisticsAndPayments/Logistics";
-import { Payments } from "@/widgets/common/ui/LogisticsAndPayments/Payments";
-import React from "react";
+import { Logistics } from '@/widgets/LogisticAndPayment/Logistics';
+import { Payments } from '@/widgets/LogisticAndPayment/Payments';
+import React from 'react';
 
 export const LogisticAndPayment = () => {
   return (
