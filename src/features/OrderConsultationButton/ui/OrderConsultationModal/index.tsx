e563ed71cc14ca6c9ec55ed592c34b0404648725
maksim.samorukov <maxim.samorukov@gmail.com>
2025-11-24 18:45:57 +0300

    feat: enhance modal and form responsiveness with dynamic sizing and adaptive styles

--- src/features/OrderConsultationButton/ui/OrderConsultationModal/index.tsx
+++ src/features/OrderConsultationButton/ui/OrderConsultationModal/index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { ModalComponent } from '@/shared/ui/Modal';
 import { OrderConsultationForm } from './ui/OrderConsultationForm';
+import { useWindowWidth } from '@/shared/hooks';
 
 type ModalComponentProps = {
   open: boolean;
@@ -12,12 +13,17 @@ export const OrderConsultationModal: React.FC<ModalComponentProps> = ({
   open,
   onClose = () => {},
 }) => {
+  const windowWidth = useWindowWidth();
+  const style =
+    windowWidth >= 420
+      ? { height: 190 }
+      : { height: 'fitContent', width: '90%' };
   return (
     <ModalComponent
       open={open}
       onClose={onClose}
       withControl={false}
-      style={{ height: 190 }}
+      style={style}
     >
       <OrderConsultationForm />
     </ModalComponent>
